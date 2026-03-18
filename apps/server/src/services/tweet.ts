34e3e249c3c7b186b346cb54c14aa8120c7f8568
Vineet Kumar Motwani <motwanivineet08@gmail.com>
2026-03-18 19:50:14 +0530

    [server] Redis re-enable

--- apps/server/src/services/tweet.ts
+++ apps/server/src/services/tweet.ts
@@ -1,5 +1,5 @@
 import { prismaClient } from "../clients/db";
-// import { redisClient } from "../clients/redis";
+import { redisClient } from "../clients/redis";
 
 export interface CreateTweetPayload {
   content: string;
@@ -9,10 +9,10 @@ export interface CreateTweetPayload {
 
 class TweetService {
   public static async createTweet(data: CreateTweetPayload) {
-    // const rateLimitFlag = await redisClient.get(
-    //   `RATE_LIMIT:TWEET:${data.userId}`
-    // );
-    // if (rateLimitFlag) throw new Error("Please wait....");
+    const rateLimitFlag = await redisClient.get(
+      `RATE_LIMIT:TWEET:${data.userId}`
+    );
+    if (rateLimitFlag) throw new Error("Please wait....");
     const tweet = await prismaClient.tweet.create({
       data: {
         content: data.content,
@@ -20,19 +20,19 @@ class TweetService {
         author: { connect: { id: data.userId } },
       },
     });
-    // await redisClient.setex(`RATE_LIMIT:TWEET:${data.userId}`, 10, 1);
-    // await redisClient.del("ALL_TWEETS");
+    await redisClient.setex(`RATE_LIMIT:TWEET:${data.userId}`, 10, 1);
+    await redisClient.del("ALL_TWEETS");
     return tweet;
   }
 
   public static async getAllTweets() {
-    // const cachedTweets = await redisClient.get("ALL_TWEETS");
-    // if (cachedTweets) return JSON.parse(cachedTweets);
+    const cachedTweets = await redisClient.get("ALL_TWEETS");
+    if (cachedTweets) return JSON.parse(cachedTweets);
 
     const tweets = await prismaClient.tweet.findMany({
       orderBy: { createdAt: "desc" },
     });
-    // await redisClient.set("ALL_TWEETS", JSON.stringify(tweets));
+    await redisClient.set("ALL_TWEETS", JSON.stringify(tweets), "EX", 10);
     return tweets;
   }
 }
