b1153f64d284f997722d568e71c0de6be70e5351
Vineet Kumar Motwani <motwanivineet08@gmail.com>
2026-04-06 04:35:05 +0530

    [client] build error fix

--- apps/client/components/FeedCard/Layout/BuzzLayout.tsx
+++ apps/client/components/FeedCard/Layout/BuzzLayout.tsx
@@ -177,7 +177,7 @@ const BuzzLayout: React.FC<BuzzLayoutProps> = (props) => {
           ) : (
             <div className="px-4 py-3 bg-slate-800 rounded-lg">
               <h1 className="my-2 text-2xl mb-5">Users you may know</h1>
-              {user?.recommendedUsers?.map((el) => (
+              {user?.recommendedUsers?.map((el: NonNullable<typeof user.recommendedUsers>[number]) => (
                 <div className="flex items-center gap-3 mt-2" key={el?.id}>
                   {el?.profileImageURL && (
                     <Image
